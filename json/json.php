db4e38f10ecd0321e92fcda363e811d67871e5fe
ning101 <193278572@qq.com>
2017-05-15 23:07:08 +0800

    ajax访问json数据

--- json/json.php
+++ json/json.php
@@ -2,10 +2,5 @@
 	header('Content-Type: application/json');
 	$arr = array('a'=>1,'b'=>2,'c'=>3);
 	$jsonstring = json_encode($arr);
-<<<<<<< HEAD
 	print_r($jsonstring);
 ?>
-=======
-	echo $jsonstring;
-?>
->>>>>>> ffd61ffd98e5bcc1c020e4d1b7380fb5a3b71062
